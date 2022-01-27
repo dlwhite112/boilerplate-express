50921e8c9ad8141b386cdc535a6e235e2803537d
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2022-01-26 18:30:16 -0600

    adding middleware function to myApp

--- myApp.js
+++ myApp.js
@@ -4,10 +4,11 @@ var app = express();
 console.log("Hello World");
 
 app.get("/", function (req, res) {
-    
   let absolutePath = __dirname + "/views/index.html";
   //   res.send("Hello Express");
   res.sendFile(absolutePath);
 });
+
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
